d286a41e2fe08ceb6273941d4efc0a5854e0fd54
songhuijie <2677060927@qq.com>
2019-12-23 14:39:38 +0800

    update14:36

--- app/Model/Goods.php
+++ app/Model/Goods.php
@@ -13,7 +13,7 @@ class Goods extends Model
      * @var array
      */
     protected $table = 'goods';
-    public $timestamps = false;
+    public $timestamps = true;
     protected $dateFormat = 'U';//使用时间戳方式添加
     /**
      * 表明模型是否应该被打上时间戳
