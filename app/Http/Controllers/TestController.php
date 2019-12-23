6a2eb9f2231b19773d21c803b15dd7b1ea8891d9
songhuijie <2677060927@qq.com>
2019-12-23 14:04:17 +0800

    s

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -27,43 +27,6 @@ class TestController extends Controller{
 //        dd($result);
 
 
-        $a = "1,概况,home,index/default,0,statistics,0,1
-2,会员,set,member/index,0,member,0,1
-3,订单,template,order/index,0,order,0,1
-4,商品,component,label/index,0,label,0,1
-5,推荐,app,recommend/index,0,recommend,0,1
-6,设置,unlink,config/index,0,setup,0,1
-7,详细概况,,index/default,1,statistics.list,0,1
-8,会员列表,,member/index,2,member.list,0,1
-9,订单列表,,order/order,3,order.list,0,1
-30,评论,voice,comment/index,0,comment,0,1
-10,推荐列表,,recommend/index,5,recommend.list,0,1
-11,程序配置,,config/index,6,setup.list,0,1
-31,评论管理,,comment/index,30,comment.list,0,1
-12,商品类型,,label/index,4,label.list,0,1
-32,权限管理,notice,admin/admin,0,admin,0,1
-33,管理员,,admin/admin,32,admin.list,0,1
-34,图片-图标管理,,config/picture,6,setup.picture,0,1
-35,图片类型,,config/type,6,config.type,0,1
-37,商品列表,,goods/index,4,label.destination,0,1
-38,定制需求,,config/essential,6,config.essential,0,1
-39,角色管理,,admin/role,32,admin.role,0,1";
-        $b = explode("\r\n",$a);
-        $datas = [];
-        $data = [];
-        foreach($b as $k=>$v){
-            echo '[';
-            echo "<br/>";
-            echo "'id'=>'".explode(',',$v)[0]."',";echo "<br/>";
-            echo "'menuname'=>'".explode(',',$v)[1]."',";echo "<br/>";
-            echo "'icon'=>'".explode(',',$v)[2]."',";echo "<br/>";
-            echo "'url'=>'".explode(',',$v)[3]."',";echo "<br/>";
-            echo "'pid'=>'".explode(',',$v)[4]."',";echo "<br/>";
-            echo "'rout'=>'".explode(',',$v)[5]."',";echo "<br/>";
-            echo "'time'=>'".explode(',',$v)[6]."',";echo "<br/>";
-            echo "'status'=>'".explode(',',$v)[7]."',";echo "<br/>";
-            echo '],';echo "<br/>";
-        }
         dd(1);
         $asset  = new Asset();
         $income  = new IncomeDetails();
