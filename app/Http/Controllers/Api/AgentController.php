8de034922e66729b97824edfa84614367adc65f1
songhuijie <2677060927@qq.com>
2019-12-27 18:38:27 +0800

    s

--- app/Http/Controllers/Api/AgentController.php
+++ app/Http/Controllers/Api/AgentController.php
@@ -163,7 +163,7 @@ class AgentController extends Controller
         foreach($lower as $k=>$v){
             $lower[$k]->user_info = $this->user->select($select)->find($v->user_id);
             $lower[$k]->count = $this->friend->LowerCount($v->parent_id);
-            $current = $this->friend->CurrentLevel($v->user_id);
+            $current = $this->friend->CurrentLevel($v->parent_id);
             $lower[$k]->user_status = $current->status;
             $lower[$k]->is_delivery = $current->is_delivery;
             $lower[$k]->contribution_amount = $this->friend->Contribution($v->parent_id);
