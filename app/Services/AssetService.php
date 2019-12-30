8be0d88b680f304b8d628f8af322cc5b4fe72811
songhuijie <2677060927@qq.com>
2019-12-30 18:21:15 +0800

    s

--- app/Services/AssetService.php
+++ app/Services/AssetService.php
@@ -11,6 +11,7 @@ use App\Events\IncomeEvent;
 use App\Libraries\Lib_const_status;
 use App\Model\Asset;
 use App\Model\IncomeDetails;
+use App\Model\WithdrawLog;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
@@ -32,6 +33,18 @@ class AssetService{
 
         $asset = new Asset();
         $income = new IncomeDetails();
+        $withdraw = new WithdrawLog();
+
+        $withdraw_time = time();
+        $withdraw_data = [
+            'user_id'=>$user_id,
+            'withdraw_type'=>1,
+            'amount'=>$amount,
+            'surplus_amount'=>0,
+            'withdraw_time'=>$withdraw_time,
+            'status'=>0,
+        ];
+        $withdraw->insert($withdraw_data);
 
         $balance = $asset->getBalance($user_id);
         $surplus_amount = $balance;
