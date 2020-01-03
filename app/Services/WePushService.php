6f5d56a8c2b0052fc7ab7fa178f20960ac83196f
songhuijie <2677060927@qq.com>
2020-01-03 14:08:41 +0800

    s

--- app/Services/WePushService.php
+++ app/Services/WePushService.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Redis;
 class WePushService{
 
 //    const TENPALATE_ID = '_qN8WDbPIumSlNoLAqkzuZTbgbLPXqjye8--09eoOOk';
-    const TENPALATE_ID = 'GOk3_dOesStpuIxF6_xTIKjkjQ8HY25GYr9ZukiDoi8';
+    const TENPALATE_ID = '_qN8WDbPIumSlNoLAqkzuZTbgbLPXqjye8--09eoOOk';
     const UEL = '/pages/index/index';
     const OPENID = 'oBNBp5DQrdMfePdUcJ8G6OtNxesw';
 
@@ -73,14 +73,19 @@ class WePushService{
             'url'=>self::UEL, //点击模板消息会跳转的链接
             'topcolor'=>"#7B68EE",
             'data'=>array(
-//                'character_string1'=>array('value'=>urlencode("123456"),'color'=>"#FF0000"),
-//                'thing2'=>array('value'=>urlencode('黄酒'),'color'=>'#FF0000'),  //keyword需要与配置的模板消息对应
-//                'thing6'=>array('value'=>urlencode(date("申通")),'color'=>'#FF0000'),
-//                'character_string7'=>array('value'=>urlencode('123'),'color'=>'#FF0000'),
-//                'phrase4' =>array('value'=>urlencode('待发货'),'color'=>'#FF0000') )
-                'keyword1' =>array('value'=>urlencode('待发货'),'color'=>'#FF0000'),
-                'keyword2' =>array('value'=>urlencode('待发货'),'color'=>'#FF0000'),
-                'keyword3' =>array('value'=>urlencode('待发货'),'color'=>'#FF0000'))
+               
+                'character_string1'=>array('value'=>urlencode("123456"),'color'=>"#FF0000"),
+                'thing2'=>array('value'=>urlencode('黄酒'),'color'=>'#FF0000'),  //keyword需要与配置的模板消息对应
+                'thing6'=>array('value'=>urlencode(date("申通")),'color'=>'#FF0000'),
+                'character_string7'=>array('value'=>urlencode('123'),'color'=>'#FF0000'),
+                'phrase4' =>array('value'=>urlencode('待发货'),'color'=>'#FF0000') )
+
+//                'keyword1' =>array('value'=>urlencode('待发货'),'color'=>'#FF0000'),
+//                'keyword2' =>array('value'=>urlencode('待发货'),'color'=>'#FF0000'),
+//                'keyword3' =>array('value'=>urlencode('待发货'),'color'=>'#FF0000')),
+
+
+
         );
 
         $json_template=json_encode($template);
@@ -88,17 +93,24 @@ class WePushService{
     }
 
     public static function curl_post($url , $data=array()){
+
+
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
+        // 以文件流形式返回
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-        // POST数据
-        curl_setopt($ch, CURLOPT_POST, 1);
-        // 把post的变量加上
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+        if (!empty($data))
+        {
+            // POST请求
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        }
+
         $output = curl_exec($ch);
         curl_close($ch);
+
+        // 返回
         return $output;
     }
 }
