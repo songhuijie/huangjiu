e68307e6e5737607e23280df0294b0a052e40c7f
songhuijie <2677060927@qq.com>
2020-01-03 11:35:48 +0800

    s

--- public/checkSignature.php
+++ public/checkSignature.php
@@ -1,26 +1,34 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2020/1/3
- * Time: 11:29
- */
+define("TOKEN", "huangjiushangcheng");
+$wx_sample=new wx_sample();
+$wx_sample->index();
+class wx_sample{
 
-$signature = $_GET["signature"];
-$timestamp = $_GET["timestamp"];
-$nonce = $_GET["nonce"];
+    //验证消息
+    public function index(){
+        $echoStr = $_GET["echostr"];
+        if($this->checkSignature()){
+            echo $echoStr;
+            exit;
+        }
+    }
 
-$token = 'huangjiushangcheng';
-$tmpArr = array($token, $timestamp, $nonce);
-sort($tmpArr, SORT_STRING);
-$tmpStr = implode( $tmpArr );
-$tmpStr = sha1( $tmpStr );
+    //检查签名
+    private function checkSignature()
+    {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode($tmpArr);
+        $tmpStr = sha1($tmpStr);
 
-
-
-
-if ($tmpStr == $signature ) {
-    return True;
-} else {
-    return False;
+        if($tmpStr == $signature){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
